5826587b1aeabdfb686ce8f3999c86806b4f0d83
Bertrand <bertrand.tang.dev@gmail.com>
2025-05-19 11:57:12 +0200

    adds header tag to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 export default function Header() {
   return (
-    <section>
+    <header>
       <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" alt="Logo" />;
       <ul>
         <li><a href="/">Page d'accueil</a></li>
       </ul>
-    </section>
+    </header>
   );
 }
