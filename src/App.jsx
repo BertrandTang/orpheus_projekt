afb43ffe7e7b716192f29feaccb9a7c183cd1dc5
Bertrand <bertrand.tang.dev@gmail.com>
2025-05-19 14:10:03 +0200

    lint

--- src/App.jsx
+++ src/App.jsx
@@ -7,33 +7,33 @@ function App() {
     <>
       <Header />
       <main>
-             <Dish
-                name="Tacos"
-                number={1}
-                title={"Tacos à l'unité"}
-                price={3}
-                img={
-                    "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
-                }
-            />
-            <Dish
-                name="Enchiladas"
-                number={2}
-                title={"Enchiladas"}
-                price={12}
-                img={
-                    "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
-                }
-            />
-            <Dish
-                name="Mole poblano"
-                number={3}
-                title={"Mole poblano"}
-                price={15}
-                img={
-                    "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
-                }
-            />
+        <Dish
+          name="Tacos"
+          number={1}
+          title={"Tacos à l'unité"}
+          price={3}
+          img={
+            "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
+          }
+        />
+        <Dish
+          name="Enchiladas"
+          number={2}
+          title={"Enchiladas"}
+          price={12}
+          img={
+            "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
+          }
+        />
+        <Dish
+          name="Mole poblano"
+          number={3}
+          title={"Mole poblano"}
+          price={15}
+          img={
+            "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
+          }
+        />
       </main>
       <Footer />
     </>
