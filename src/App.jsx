738cbdcd8778f2381c3d9907aaa291a7a2efbd93
Bertrand <bertrand.tang.dev@gmail.com>
2025-05-20 09:51:54 +0200

    adds activity 11

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,18 @@ import Col from 'react-bootstrap/Col';
 
 function Dishes() {
   const dishes = [
-    { id: 1, title: "Tacos à l'unité", price: 3, img: "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg", isNew: true },
-    { id: 2, title: "Enchiladas", price: 12, img: "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg", isNew: false },
-    { id: 3, title: "Mole poblano", price: 15, img: "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg", isNew: false },
+    { id: 1, title: "Tacos à l'unité", price: 3, stock: 12, img: "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg", isNew: true },
+    { id: 2, title: "Enchiladas", price: 12, stock: 0, img: "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg", isNew: false },
+    { id: 3, title: "Mole poblano", price: 15, stock: 5, img: "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg", isNew: false },
   ];
+
+  const inStockDishes = dishes.filter(dish => dish.stock > 0);
+
   return (
     <Container as="main">
       <Row>
         <ul>
-          {dishes.map(dish => (
+          {inStockDishes.map(dish => (
             <Col md={4}>
               <li key={dish.id}>
                 <Dish
